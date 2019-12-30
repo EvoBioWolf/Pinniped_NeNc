abe77197ff4e79d645d987755cd6d69a2edeca01
Saurabh-Dilip Pophaly <spophaly@mpipz.mpg.de>
2019-12-30 13:10:44 +0100

    Script generated by make -n (dryrun)

--- ClustRAD/insilico_digest/DryRun.sh
+++ ClustRAD/insilico_digest/DryRun.sh
@@ -1,13 +1,13 @@
 #Creating 2bit from fasta
-faToTwoBit GCF_000321225.1_Oros_1.0_genomic.fa GCF_000321225.1_Oros_1.0_genomic.2bit
-#getting oligo sites for genome file GCF_000321225.1_Oros_1.0_genomic
-oligoMatch oligo.fa GCF_000321225.1_Oros_1.0_genomic.2bit GCF_000321225.1_Oros_1.0_genomic.resbed
+faToTwoBit Odo_ros.fa Odo_ros.2bit
+#getting oligo sites for genome file Odo_ros
+oligoMatch oligo.fa Odo_ros.2bit Odo_ros.resbed
 #Get individual enzyme cut bed
-grep "	SbFI\+" GCF_000321225.1_Oros_1.0_genomic.resbed > SbFI.bed
+grep "	SbFI\+" Odo_ros.resbed > SbFI.bed
 #Get individual enzyme cut bed
-grep "	MseI\+" GCF_000321225.1_Oros_1.0_genomic.resbed > MseI.bed
+grep "	MseI\+" Odo_ros.resbed > MseI.bed
 #get clusters using bedtools 
 bedtools window -a SbFI.bed -b MseI.bed -w 500 > SbFI__MseI.join
 #joining clusters
-perl cluster.pl SbFI__MseI.join > GCF_000321225.1_Oros_1.0_genomic_SbFI__MseI.clusters
-rm GCF_000321225.1_Oros_1.0_genomic.resbed GCF_000321225.1_Oros_1.0_genomic.2bit
+perl cluster.pl SbFI__MseI.join > Odo_ros_SbFI__MseI.clusters
+rm Odo_ros.resbed Odo_ros.2bit
